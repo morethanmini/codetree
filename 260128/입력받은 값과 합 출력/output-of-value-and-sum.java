29af7a29b1dfdb11954d6909b030d56692bd8229
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-01-28 19:29:02 +0900

    Title: 입력받은 값과 합 출력, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260128/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+
+        System.out.printf("%d %d %d", A, B, A+B);
+
+
+    }
+}
