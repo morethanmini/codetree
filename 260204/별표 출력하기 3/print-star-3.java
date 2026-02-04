440e4bd97276f2c1957176640aefd39abe60c66f
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-04 14:21:08 +0900

    Title: 별표 출력하기 3, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260204/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        // int b = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<a; i++) {
+            for(int j=0; j<i; j++) {
+                System.out.print("  ");
+            }
+            
+            for(int j=0; j<2*(a-i)-1; j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+// a = 5
+// 별 공백
+// 9  0
+// 7  1
+// 5  2
+// 3  3
+// 1  4
+
+
+
+    }
+}
