734e783b775891fb8dba57c91ce08c5345b2478e
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-06 00:57:49 +0900

    Title: 반복 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260206/반복 출력하기/repeated-output.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void print() {
+        System.out.println("12345^&*()_");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        for(int i=0; i<n; i++)
+            print();
+    }
+}
