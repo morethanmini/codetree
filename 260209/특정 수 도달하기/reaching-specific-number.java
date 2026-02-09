a125165cdc99e8e6fbb8efd3245fc10f8056cc29
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-09 22:34:13 +0900

    Title: 특정 수 도달하기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260209/특정 수 도달하기/reaching-specific-number.java
+++ 260209/특정 수 도달하기/reaching-specific-number.java
@@ -3,32 +3,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> lst = new ArrayList<>();
-
         String[] arr = br.readLine().split(" ");
-        int sum = 0;
-        int cnt = 0;
 
-        // for문 돌면서 앞에서부터 검사
-        for(int i=0; i<arr.length; i++) {
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
             int value = Integer.parseInt(arr[i]);
 
-            // 250 이상 나오면 그 이전까지만 사용
-            if (value >= 250)
+            if (value >= 250) {
                 break;
+            }
 
             sum += value;
-            cnt++;
-
+            count++;
         }
 
-        // 끝까지 없으면 10개 모두 사용
-        double avg = (cnt == 0) ? 0 : (double) sum / cnt;
-
-        // 합과 평균 출력
+        double avg = (count == 0) ? 0 : (double) sum / count;
         System.out.println(sum + " " + avg);
-
     }
 }
