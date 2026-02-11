e80121d3fbee18f0c186f2982cf1417913fa0bc1
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-11 17:07:06 +0900

    Title: 파스칼의 삼각형, Time: 88ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 260211/파스칼의 삼각형/pascals-triangle.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][N];
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<1; j++) {
+                arr[i][j] = 1;
+            }
+        }
+        arr[1][1] = 1;
+
+
+        // 2. 규칙
+        for (int i=1; i<N; i++) {
+            for (int j=1; j<N; j++) {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+
+
+        // 출력
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (arr[i][j] != 0)
+                    System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
