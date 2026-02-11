154745e3afe2248efefc2dd501ec7c51e314360e
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-11 17:45:39 +0900

    Title: 동전이 있는 위치, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260211/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][N];
+
+        for (int T=0; T<M; T++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[r-1][c-1] = 1;
+        }
+
+
+        // 출력
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
