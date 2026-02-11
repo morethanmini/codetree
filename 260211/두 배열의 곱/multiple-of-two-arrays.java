d068824a90bf976ae4c2ef536e87b62e09dffcbd
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-11 13:59:00 +0900

    Title: 두 배열의 곱, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260211/두 배열의 곱/multiple-of-two-arrays.java
+++ 260211/두 배열의 곱/multiple-of-two-arrays.java
@@ -2,50 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    public static void main(String[] args) {
 
         int[][] arr1 = new int[3][3];
         int num1 = 1;
 
-        for (int i=0; i<3; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            for (int j=0; j<3; j++) {
-                arr1[i][j] = num1;
-                num1 += 1;
+        // arr1 채우기
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr1[i][j] = num1++;
             }
         }
 
         int[][] arr2 = new int[3][3];
         int num2 = 2;
 
-        for (int i=0; i<3; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            for (int j=0; j<3; j++) {
-                arr2[i][j] = num2;
-                num2 += 1;
+        // arr2 채우기
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr2[i][j] = num2++;
             }
         }
 
-        // System.out.println(Arrays.deepToString(arr1));
-        // System.out.println(Arrays.deepToString(arr2));
-
         int[][] arr3 = new int[3][3];
 
-        for (int i=0; i<3; i++) {
-            for (int j=0; j<3; j++) {
+        // 원소별 곱(element-wise multiplication)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 arr3[i][j] = arr1[i][j] * arr2[i][j];
-
                 System.out.print(arr3[i][j] + " ");
             }
             System.out.println();
         }
-
-        // System.out.println(Arrays.deepToString(arr3));
-
-
     }
 }
