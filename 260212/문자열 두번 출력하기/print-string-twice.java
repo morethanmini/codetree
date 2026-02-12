7c8e3384fcf8b903e92825a4918a4d4ec9f5c5d4
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-12 11:52:05 +0900

    Title: 문자열 두번 출력하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260212/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        // System.out.println(s);
+
+        for (int i=0; i<2; i++) {
+            System.out.println(s);
+        }
+    }
+}
