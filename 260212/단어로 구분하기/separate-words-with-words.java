5a15ff2fcfd608a7e0f6beba49433f31218f70c8
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-12 14:42:47 +0900

    Title: 단어로 구분하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260212/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            String s = st.nextToken();
+
+            System.out.println(s);
+        }
+
+
+    }
+}
