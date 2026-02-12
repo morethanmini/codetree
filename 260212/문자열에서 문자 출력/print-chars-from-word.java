cd8f641be77076fdf53e4ebbd1bb19f1369efbc5
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-12 15:28:58 +0900

    Title: 문자열에서 문자 출력, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260212/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        for(int i=0; i<s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
