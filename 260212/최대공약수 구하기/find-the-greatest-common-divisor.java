5c3acb7c05ed22f81f008dea188d1ebb339eac75
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-12 17:03:45 +0900

    Title: 최대공약수 구하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260212/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(gcd(n, m));
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
