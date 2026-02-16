a1333239a9d28590e4c8d6050694b33b2efd66fa
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-16 12:51:07 +0900

    Title: 각 자리 숫자의 제곱, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 260216/각 자리 숫자의 제곱/square-of-each-digit.java
+++ 260216/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static int R(int n) {
+        if (n<10)
+            return n * n;
         
+        // System.out.println(n%10);
+        return R(n/10) + (n%10) * (n%10);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // Please write your code here.
+        System.out.print(R(n));
     }
 }
