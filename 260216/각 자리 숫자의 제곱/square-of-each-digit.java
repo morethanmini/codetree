372c25aca4859e4bde088ea06e3f0f4b8f202648
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-16 12:38:13 +0900

    Title: 각 자리 숫자의 제곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260216/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static int R(int n) {
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+    }
+}
