1ab4791b36be183fd4b2775939ff1ea3eb2115ee
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-16 14:01:31 +0900

    Title: Factorial, Time: 135ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260216/Factorial/factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int R(int n) {
+        if (n==1)
+            return 1;
+        
+        return n * R(n-1);
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.print(R(n));
+    }
+}
