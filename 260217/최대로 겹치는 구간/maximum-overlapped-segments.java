e467cfd10cededf2eeda092fd6b5d2f3b8d476b5
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-17 14:42:01 +0900

    Title: 최대로 겹치는 구간, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 260217/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 260217/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -1,36 +1,45 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
+
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
+
+    public static int[] checked = new int[MAX_R + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        int[] x1 = new int[n];
-        int[] x2 = new int[n];
+        // 입력
+        n = sc.nextInt();
 
-        int maxCoord = 0;
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             x1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
-            maxCoord = Math.max(maxCoord, x2[i]);
+
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            x2[i] += OFFSET;
         }
 
-        int[] arr = new int[maxCoord];
-
-        // 구간 카운팅
-        for (int i = 0; i < n; i++) {
-            for (int j = x1[i]; j <= x2[i]; j++) {
-                arr[j - 1]++;
-            }
-        }
-
-        // 최대 겹침 개수 계산
-        int answer = 0;
-        for (int i = 0; i < arr.length; i++) {
-            answer = Math.max(answer, arr[i]);
-        }
-
-        System.out.println(answer);
+        // 구간을 칠해줍니다.
+        // 구간 단위로 진행하는 문제이므로
+        // x2[i]에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < n; i++)
+            for(int j = x1[i]; j < x2[i]; j++)
+                checked[j]++;
+        
+        // 최댓값을 구합니다.
+        int max = 0;
+        for(int i = 0; i <= MAX_R; i++)
+            if(checked[i] > max)
+                max = checked[i];
+        
+        // 출력
+        System.out.print(max);
     }
 }
