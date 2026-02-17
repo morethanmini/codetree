8479b0f3d17af6bcf95c4d7e4aa1f87a2315aa2d
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-17 14:11:13 +0900

    Title: 진수 to 진수, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260217/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+        // Please write your code here.
+
+        char[] arr = N.toCharArray();
+        int n = 0;
+
+        for (int i=0; i<arr.length; i++) {
+            n = n * A + (arr[i] - '0');
+        }
+
+        List<Integer> lst = new ArrayList<>();
+        while (true) {
+            if (n<2) {
+                lst.add(n);
+                break;
+            }
+
+            lst.add(n%B);
+            n /= B;
+        }
+
+        for (Integer i : lst) {
+            System.out.print(i);
+        }
+    }
+}
