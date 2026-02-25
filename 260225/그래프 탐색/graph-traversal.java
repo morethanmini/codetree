2e27b49f597e6d8bc02fb24cdd078d1076f5a98f
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-25 23:21:39 +0900

    Title: 그래프 탐색, Time: 464ms, Memory: 24MB, Status: Passed - Codetree

---
+++ 260225/그래프 탐색/graph-traversal.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+    public static final int MAX_NUM = 1000;
+    public static int n, m;
+
+    public static ArrayList<Integer>[] graph = new ArrayList[MAX_NUM+1];
+    public static boolean[] visited = new boolean[MAX_NUM+1];
+    public static int vertexCnt = 0;
+
+    public static void DFS(int vertex) {
+        for (int i=0; i<graph[vertex].size(); i++) {
+            int currV = graph[vertex].get(i);
+            if(!visited[currV]) {
+                visited[currV] = true;
+                vertexCnt++;
+                DFS(currV);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        while (m-- >0) {
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+
+            graph[v1].add(v2);
+            graph[v2].add(v1);
+        }
+
+        visited[1] = true;
+        DFS(1);
+
+        System.out.println(vertexCnt);
+        
+    }
+}
