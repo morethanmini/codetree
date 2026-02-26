95c7e66c4cc7c9ef8fa00dfe61b8eafc5424efcd
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-26 12:00:34 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 260226/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 260226/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -1,57 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int n, m;
-    public static final int MAX_NUM = 100;
+	public static int n, m;
+	public static int MAX_NUM = 100;
+	public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+	public static int[][] visited = new int[MAX_NUM][MAX_NUM];
+	
+	public static boolean inRange(int x, int y) {
+		return 0<=x && x<n && 0<=y && y<m;
+	}
+	
+	public static boolean run(int x, int y) {
+		if (!inRange(x, y)) return false;
+		if (visited[x][y] == 1 || grid[x][y] == 0) return false;
+		return true;
+	}
+	
+	public static void DFS(int x, int y) {
+		int[] dx = {1, 0};
+		int[] dy = {0, 1};
+		
+		visited[x][y] = 1;
+		
+		for (int i=0; i<2; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if (run(nx, ny)) {
+				DFS(nx, ny);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		for (int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j=0; j<m; j++) {
+				grid[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		visited[0][0] = 1;
+		DFS(0,0);
+		
+		System.out.println(visited[n-1][m-1]);
+	}
 
-    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
-    public static int[][] visited = new int[MAX_NUM][MAX_NUM];
-
-    public static boolean inRange(int x, int y) {
-        return 0 <= x && x < n && 0 <= y && y < m;
-    }
-
-    public static boolean run(int x, int y) {
-        if (!inRange(x, y)) return false;
-        if (visited[x][y] == 1 || grid[x][y] == 0) return false;
-        return true;
-    }
-
-    public static void DFS(int x, int y) {
-        int[] dx = {1, 0};
-        int[] dy = {0, 1};
-
-        visited[x][y] = 1;
-
-        for (int i = 0; i < 2; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (run(nx, ny)) {
-                DFS(nx, ny);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                grid[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        visited[0][0] = 1;
-        DFS(0, 0);
-
-        System.out.println(visited[n-1][m-1]);
-    }
 }
