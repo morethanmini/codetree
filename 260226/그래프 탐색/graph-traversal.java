1214773e4767226adae957f146568f454de8a425
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-26 10:28:33 +0900

    Title: 그래프 탐색, Time: 201ms, Memory: 14MB, Status: Passed - Codetree

--- 260226/그래프 탐색/graph-traversal.java
+++ 260226/그래프 탐색/graph-traversal.java
@@ -1,71 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n, m;
-    
-    // 큰 배열 만들기
-    public static final int MAX_NUM = 1000;
+	public static int n, m;
+	
+	public static final int MAX_NUM = 1000;
+	public static ArrayList<Integer>[] graph = new ArrayList[MAX_NUM+1];
+	public static boolean[] visited = new boolean[MAX_NUM+1];
+	public static int vertexCnt = 0;
+	
+	public static void DFS(int vertex) {
+		for (int i=0; i<graph[vertex].size(); i++) {
+			int currV = graph[vertex].get(i);
+			
+			if (!visited[currV]) {
+				visited[currV] = true;
+				vertexCnt++;
+				DFS(currV);
+			}
+		}
+	}
 
-    // 어레이 리스트를 담는 graph 배열 선언
-    public static ArrayList<Integer>[] graph = new ArrayList[MAX_NUM+1];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		for (int i=1; i<=n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		while (m-- >0) {
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
+			
+			graph[v1].add(v2);
+			graph[v2].add(v1);
+		}
+		
+		visited[1] = true;
+		DFS(1);
+		
+		System.out.println(vertexCnt);
 
-    // 방문 초기화
-    public static boolean[] visited = new boolean[MAX_NUM+1];
-    public static int vertexCnt = 0;
+	}
 
-    // DFS(정점)
-    public static void DFS(int vertex) {
-
-        // 정점 배열 사이즈만큼
-        for (int i=0; i<graph[vertex].size(); i++) {
-
-            // 현재 정점 가져오기
-            int currV = graph[vertex].get(i);
-
-            // 현재 노드 방문하지 않으면
-            if (!visited[currV]) {
-
-                // 방문처리
-                visited[currV] = true;
-
-                // 카운트 변수 세기
-                vertexCnt++;
-
-                // 재귀
-                DFS(currV);
-            }
-        }
-
-    }
-            
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-
-        // n만큼 graph 만들기
-        for (int i=1; i<=n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
-        // 각 정점에 대한 간선 저장(양방향)
-        while (m-- > 0) {
-            int v1 = sc.nextInt();
-            int v2 = sc.nextInt();
-
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-        }
-
-        // 시작 노드 방문 처리
-        visited[1] = true;
-
-        // 시작 노드 DFS 넣기
-        DFS(1);
-
-        System.out.println(vertexCnt);
-
-        
-    }
 }
