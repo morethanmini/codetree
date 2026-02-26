f194f415bed3e1fcda7ff39009b914b3fa664c84
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2026-02-26 20:10:15 +0900

    Title: 안전 지대, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260226/안전 지대/comfort-zone.java
+++ 260226/안전 지대/comfort-zone.java
@@ -2,77 +2,90 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static final int DIR_NUM = 4;
+    public static final int MAX_M = 50;
+    public static final int MAX_N = 50;
+    
+    public static int n, m;
+    public static int[][] grid = new int[MAX_N][MAX_M];
+    public static boolean[][] visited = new boolean[MAX_N][MAX_M];
+    public static int zoneNum;
+    public static int maxHeight = 0;
 
-    static int n, m;
-    static int[][] grid;
-    static boolean[][] visited;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    static boolean inRange(int x, int y) {
+    public static void initializeVisited() {
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                visited[i][j] = false;
+    }
+    
+    public static boolean inRange(int x, int y) {
         return x >= 0 && x < n && y >= 0 && y < m;
     }
-
-    static void dfs(int x, int y, int rainHeight) {
-        visited[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (inRange(nx, ny)
-                    && !visited[nx][ny]
-                    && grid[nx][ny] > rainHeight) {
-                dfs(nx, ny, rainHeight);
+    
+    public static boolean canGo(int x, int y, int k) {
+        if(!inRange(x, y)) return false;
+        if(visited[x][y] || grid[x][y] <= k) return false;
+        return true;
+    }
+    
+    public static void DFS(int x, int y, int k) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        
+        for(int dir = 0; dir < DIR_NUM; dir++) {
+            int newX = x + dx[dir];
+            int newY = y + dy[dir];
+    
+            if(canGo(newX, newY, k)){
+                visited[newX][newY] = true;
+                DFS(newX, newY, k);
             }
         }
     }
+    
+    public static void getZoneNum(int k) {
+        zoneNum = 0;
+        initializeVisited();
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) {
+                if(canGo(i, j, k)) {
+                    visited[i][j] = true;
+                    zoneNum++;
+                    DFS(i, j, k);
+                }
+            }
+    }
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        int maxZoneNum = -1; 
+        int answerK = 0;
+
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        grid = new int[n][m];
-
-        int maxHeight = 0;
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++) {
                 grid[i][j] = Integer.parseInt(st.nextToken());
                 maxHeight = Math.max(maxHeight, grid[i][j]);
             }
         }
 
-        int maxSafeAreaCount = 0;
-        int bestRainHeight = 0;
+        // k = 0부터 시작
+        for(int k = 0; k <= maxHeight; k++){
+            getZoneNum(k);
 
-        for (int rain = 0; rain <= maxHeight; rain++) {
-
-            visited = new boolean[n][m];
-            int safeAreaCount = 0;
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-
-                    if (!visited[i][j] && grid[i][j] > rain) {
-                        dfs(i, j, rain);
-                        safeAreaCount++;
-                    }
-                }
-            }
-
-            if (safeAreaCount >= maxSafeAreaCount) {
-                maxSafeAreaCount = safeAreaCount;
-                bestRainHeight = rain;
+            if(zoneNum >= maxZoneNum) {
+                maxZoneNum = zoneNum;
+                answerK = k;
             }
         }
 
-        System.out.println(maxSafeAreaCount + " " + bestRainHeight);
+        System.out.print(answerK + " " + maxZoneNum);
     }
 }
